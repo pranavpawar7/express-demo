006df8cb9d937ee0e7b4fafc9e1313eaab602ee5
niharbijva1463 <nihar@coronation.in>
2025-07-02 11:07:42 +0530

    host on local IP

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const userRoutes = require("./routes/userRoutes");
 
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
 
-app.use('/api', userRoutes);
+app.use("/api", userRoutes);
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+app.listen(3000, "192.168.1.199", () => {
     console.log(`Server running at localhost ${port}`);
 });
+// app.listen(port, () => {
+//     console.log(`Server running at localhost ${port}`);
+// });
--- config/db.js
+++ config/db.js
@@ -1,24 +1,24 @@
-const mysql = require("mysql2")
-
-const db = mysql.createConnection({
-	host: "sql12.freesqldatabase.com",
-	user: "sql12787731",
-	password: "7lFYErHag7",
-	database: "sql12787731",
-	multipleStatements: true,
-})
+const mysql = require("mysql2");
 
 // const db = mysql.createConnection({
-//     host:'localhost',
-//     user:'root',
-//     password:'',
-//     database:'basic_table',
-//     multipleStatements:true
-// });
+// 	host: "sql12.freesqldatabase.com",
+// 	user: "sql12787731",
+// 	password: "7lFYErHag7",
+// 	database: "sql12787731",
+// 	multipleStatements: true,
+// })
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "basic_table",
+    multipleStatements: true,
+});
 
 db.connect((err) => {
-	if (err) throw err
-	console.log("MySQL Connected")
-})
+    if (err) throw err;
+    console.log("MySQL Connected");
+});
 // test demo
-module.exports = db
+module.exports = db;
